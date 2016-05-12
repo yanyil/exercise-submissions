702609a8cfd931420c84b8f7e6fda21d822b063e
Yan-Yi Li <liyanyi@gmail.com>
2016-05-12 11:07:23 +0100

    complete anagram

---
+++ csharp/anagram/Anagram.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace anagram
+{
+	public class Anagram
+	{
+		private string _word;
+
+		public Anagram (string word)
+		{
+			_word = word.ToLower ();
+		}
+
+		public string[] Match (string[] words)
+		{
+			var matches = new List<string> ();
+			foreach (var word in words) {
+				if (IsAnagram (word)) {
+					matches.Add (word);
+				}
+			}
+			return matches.ToArray ();
+		}
+
+		private bool IsAnagram (string candidate)
+		{
+			return _word != candidate.ToLower () && SortWord (_word) == SortWord (candidate.ToLower ());
+		}
+
+		private static string SortWord (string word)
+		{
+			return String.Concat (word.OrderBy (c => c));
+		}
+	}
+}
