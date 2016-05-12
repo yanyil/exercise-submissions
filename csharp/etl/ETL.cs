dd33cddd194c9e154c8d0663bf0e4df12f523f11
Yan-Yi Li <liyanyi@gmail.com>
2016-05-12 07:30:53 +0100

    complete etl

---
+++ csharp/etl/ETL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace etl
+{
+	public class ETL
+	{
+		public static Dictionary<string, int> Transform (Dictionary<int, IList<string>> oldScores)
+		{
+			var newScores = new Dictionary<string, int>();
+			foreach (var item in oldScores) {
+				foreach (var letter in item.Value) {
+					newScores.Add (letter.ToLower(), item.Key);
+				}
+			}
+			return newScores;
+		}
+	}
+}
+
