7468382a977db8c2c459c6dfd8943266897e6102
Yan-Yi Li <liyanyi@gmail.com>
2016-05-13 17:35:49 +0100

    complete hamming

---
+++ csharp/hamming/Hamming.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace hamming
+{
+	public class Hamming
+	{
+		public static int Compute (string strandA, string strandB)
+		{
+			int distance = 0;
+			for (int i = 0; i < strandA.Length; i++)
+			{
+				if (strandA[i] != strandB[i])
+				{
+					distance++;
+				}
+			}
+			return distance;
+		}
+	}
+}
+
