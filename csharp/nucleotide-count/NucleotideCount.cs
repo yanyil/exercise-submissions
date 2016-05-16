5c308d31b38882baa478fd67de4d1130aaec6316
Yan-Yi Li <liyanyi@gmail.com>
2016-05-16 15:53:42 +0100

    complete nucleotide-count

---
+++ csharp/nucleotide-count/NucleotideCount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace dna
+{
+	public class DNA
+	{
+		public IDictionary<char, int> NucleotideCounts { get; }
+
+		public DNA(string strand)
+		{
+			NucleotideCounts = new Dictionary<char, int>{ { 'A', 0 }, { 'T', 0 }, { 'C', 0 }, { 'G', 0 } };
+			foreach (char c in strand)
+			{
+				NucleotideCounts [c]++;
+			}
+		}
+
+		public int Count (char nucleotide)
+		{
+			int count;
+			if (!NucleotideCounts.TryGetValue(nucleotide, out count))
+			{
+				throw new InvalidNucleotideException ();
+			}
+			return count;
+		}
+	}
+}
+
+public class InvalidNucleotideException : Exception {};
