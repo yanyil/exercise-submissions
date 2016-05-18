f03d570cab3e4d5fc8c6a2bb8d890620ff86b4fd
Yan-Yi Li <liyanyi@gmail.com>
2016-05-18 23:33:08 +0100

    complete robot-name

---
+++ csharp/robot-name/RobotName.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace robot
+{
+	public class Robot
+	{
+		private static Random _random = new Random ();
+
+		public string Name { get; private set; }
+
+		public Robot ()
+		{
+			Reset ();
+		}
+
+		public void Reset ()
+		{
+			Name = GenerateName ();
+		}
+
+		private string GenerateNumber ()
+		{
+			return _random.Next (1000).ToString ("000");
+		}
+
+		private char GenerateCharacter ()
+		{
+			return (char)('A' + _random.Next (26));
+		}
+
+		private string GenerateName()
+		{
+			return String.Format ("{0}{1}{2}", GenerateCharacter (), GenerateCharacter (), GenerateNumber ());
+		}
+	}
+}
