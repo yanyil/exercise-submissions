4165747cb91b780deae1ba1a369030d93cc61a75
Yan-Yi Li <liyanyi@gmail.com>
2016-05-19 11:14:57 +0100

    complete sum-of-multiples

---
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace sumofmultiples
+{
+	public class SumOfMultiples
+	{
+		public static int To (int[] numbers, int max)
+		{
+			return Enumerable.Range (1, max - 1).
+				Where (i => IsMultiple (numbers, i)).Sum ();
+		}
+
+		private static bool IsMultiple (int[] numbers, int i)
+		{
+			return numbers.Any(n => i % n ==0);
+		}
+	}
+}
