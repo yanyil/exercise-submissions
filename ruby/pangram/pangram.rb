ddd44857dd0b63ed9394420b12d56264d4477035
Yan-Yi Li <liyanyi@gmail.com>
2016-05-26 21:57:02 +0100

    refactor ruby pangram so string is only downcased once

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -2,6 +2,7 @@ class Pangram
   VERSION = 1
 
   def self.is_pangram?(string)
-    ("a".."z").all? { |letter| string.downcase.include?(letter) }
+    downcased_string = string.downcase
+    ("a".."z").all? { |letter| downcased_string.include?(letter) }
   end
 end
