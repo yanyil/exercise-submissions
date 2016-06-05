a1191075d1ae969f1d10ce7c1f5935d3e072e619
Yan-Yi Li <liyanyi@gmail.com>
2016-06-05 16:42:13 +0100

    refactor ruby nth-prime - test for composite numbers instead of primes

--- ruby/nth-prime/nth_prime.rb
+++ ruby/nth-prime/nth_prime.rb
@@ -5,17 +5,15 @@ class Prime
     number = 1
     while counter < n
       number += 1
-      counter += 1 if self.prime?(number)
+      counter += 1 unless composite?(number)
     end
     number
   end
 
-  private
-
-  def self.prime?(n)
-    (2..Math.sqrt(n)).each do |i|
-      return false if n % i == 0
+  def self.composite?(n)
+    (2..Math.sqrt(n)).any? do |i|
+      n % i == 0
     end
-    true
   end
+  private_class_method :composite?
 end
